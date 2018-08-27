d414db171c09e3b96faaadda64f2915a2732267e
kimdj2 <1222kdj@gmail.com>
2018-08-28 01:04:20 +0900

    commit4

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,8 @@ CarrierWave.configure do |config|
         :provider =>                'AWS',                                      # required
         :aws_access_key_id =>       ENV["S3_ACCESS_KEY"],                              # required
         :aws_secret_access_key =>   ENV["S3_SECRET_KEY"],                      # required
-        :region =>                  'ap-northeast-1',                           
+        :region =>                  'ap-northeast-1',
+        :endpoint =>                'https://s3-ap-northeast-1.amazonaws.com'                     
     }
     config.fog_directory  = ENV['S3_BUCKET']              # required
     config.fog_public     = true                                              # optional, defaults to true
