dbc5e8b68b55cb67eb36de57331207bc22166881
kimdj2 <1222kdj@gmail.com>
2018-08-27 23:54:17 +0900

    commit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,11 @@
 #/app/uploaders/image_uploader.rb
 class ImageUploader < CarrierWave::Uploader::Base
     
-    storage :file
+    if Rails.env.production?
+        storage :fog
+    else
+        storage :file
+    end
 
     def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,23 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => 'ap-northeast-1',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+
+    config.fog_directory     =  ENV['S3_BUCKET']
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
+end
