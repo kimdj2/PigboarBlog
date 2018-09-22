3d1fc25a86db401b74bdb59f98cef6b9d7775b39
kimdj2 <1222kdj@gmail.com>
2018-09-23 03:48:29 +0900

    main

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,8 @@ class MainController < ApplicationController
     before_action :setTagAndCategory
     def index
         @boards = Board.all.order(created_at:"DESC").limit(6)
-        #@boards_ino = Board.where("author='イノシシ'").order(created_at:"DESC").limit(6)
-        #@boards_buta = Board.where("author='豚'").order(created_at:"DESC").limit(6)
+        @boards_ino = Board.where("author='イノシシ'").order(created_at:"DESC").limit(6)
+        @boards_buta = Board.where("author='豚'").order(created_at:"DESC").limit(6)
         render 'main/main'
     end
     def rss
