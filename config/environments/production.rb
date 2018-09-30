39d3530ef3261969eeadd184ec3d40e189566b2f
kimdj2 <1222kdj@gmail.com>
2018-10-01 00:41:26 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,9 +87,6 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
   host = 'https://pigboars.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
@@ -103,6 +100,4 @@ Rails.application.configure do
     :authentication => 'login',
     :tls => true,
   }
-
-  
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( '.ttf' ) 
 Rails.application.config.assets.precompile += %w( tinymce/plugins/uploadimage/langs/ja.js )
 
 
