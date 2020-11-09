ab25e1775d945c45c24fd4772d8bb783ae69d8a9
kimdj2 <1222kdj@gmail.com>
2020-11-10 01:30:25 +0900

    add image uploader

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,19 +2,13 @@ class ImagesController < ApplicationController
   protect_from_forgery :except => [:image_upload]
   before_action :authenticate_request!, only: [:image_upload]
 
-  def image_upload
+  def upload_image
     image_param = params[:image]
-    if image_param["file"].present?
-      @image_path = Imgur.new('97261fb9958613a').anonymous_upload(image_param["file"])
+    if image_param.present?
+      @image_path = Imgur.new('97261fb9958613a').anonymous_upload(image_param)
     end
     render json: {image_url: @image_path}, status: :ok
   end
-
-
-  def image_upload
-    @image = Image.build.params(image_params)
-  end
-
 end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     get 'archive', {:format => :json}, to: 'boards#archive'
     get 'recent_posts', {:format => :json}, to: 'boards#recent_posts'
     post 'auth_user', to: 'auth#authenticate_user'
+    post 'upload_image', to: 'images#upload_image'
     get 'get_auth', to: 'auth#get_auth'
+
   end
 end
