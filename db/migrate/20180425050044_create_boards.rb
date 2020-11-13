ebab534784e9fdc4fc1ccaa528f631f887f9c5ff
kim dongjin <1222kdj@gmail.com>
2020-11-14 00:36:03 +0900

    temp migrate up

--- db/migrate/20180425050044_create_boards.rb
+++ db/migrate/20180425050044_create_boards.rb
@@ -1,14 +1,14 @@
 class CreateBoards < ActiveRecord::Migration[5.1]
-  def change
-    create_table :boards do |t|
-      t.string :title
-      t.text :contents
-      t.string :author
-      t.string :image_path
+  # def change
+  #   create_table :boards do |t|
+  #     t.string :title
+  #     t.text :contents
+  #     t.string :author
+  #     t.string :image_path
 
-      t.timestamps
-    end
-    execute "ALTER TABLE boards ADD FOREIGN KEY (author) references users(username);"
+  #     t.timestamps
+  #   end
+  #   execute "ALTER TABLE boards ADD FOREIGN KEY (author) references users(username);"
 
-  end
+  # end
 end
