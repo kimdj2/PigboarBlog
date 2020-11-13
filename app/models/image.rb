c41db29c156b32a5b77b4fc9fbd60a57d7cbeb95
kim dongjin <1222kdj@gmail.com>
2020-11-13 23:54:40 +0900

    delete production credential

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,5 @@
 class Image < ApplicationRecord
     #アップロード
-    mount_uploader :file, ImageUploader
     before_destroy :destroy_assets
     #ファイル情報が残っている場合はファイル情報を削除し、保存する。
     def destroy_assets
