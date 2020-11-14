4bbf47ab9df8ee288f4f62fd4dcb04de80a6ac3b
kim dongjin <1222kdj@gmail.com>
2020-11-15 01:46:19 +0900

    add cors available domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # 許可するドメイン
-    origins "localhost", "https://pullog.herokuapp.com"
+    origins "localhost", "https://pullog.herokuapp.com", "https://www.pullog.tokyo"
     # 許可するヘッダとメソッドの種類
     resource "*",
               headers: :any,
