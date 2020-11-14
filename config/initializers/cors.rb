b977a806114d88850f33512150db3db440dbfa6b
kim dongjin <1222kdj@gmail.com>
2020-11-14 20:19:59 +0900

    modify setting cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # 許可するドメイン
-    origins "localhost", "pullog.herokuapp.com"
+    origins "localhost", "https://pullog.herokuapp.com"
     # 許可するヘッダとメソッドの種類
     resource "*",
               headers: :any,
